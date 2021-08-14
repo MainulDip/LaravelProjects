03d515d72af54575fff866931773b8ad9b46f093
mainuldip <mainuldip@gmail.com>
2021-08-13 20:13:55 -0500

    tailwind working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Social Media</title>
 
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
-<body>
+<body class="bg-gray-400">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Friends</a></li>
+            <li><a href="">Blog</a></li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 
